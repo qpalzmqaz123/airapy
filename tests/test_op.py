434d2526eb21e119c951e2ff6f50362eb447e7ce
wangqj <qpalzmqaz123@gmail.com>
2018-07-31 17:23:20 +0800

    add some test

--- tests/test_op.py
+++ tests/test_op.py
@@ -82,3 +82,55 @@ class TestBinOp(BaseTest):
         assert self.vm.stack[0] == False
         assert self.vm.stack[1] == True
         assert self.vm.reg.SP == 2
+
+class TestComplexOp(BaseTest):
+
+    def test_1(self):
+        '''1 + 2 + 3'''
+        assert self.vm.stack[0] == 6
+
+    def test_2(self):
+        '''1 + 2 * 3'''
+        assert self.vm.stack[0] == 7
+
+    def test_3(self):
+        '''(1 + 2) * 3'''
+        assert self.vm.stack[0] == 9
+
+class TestAssign(BaseTest):
+
+    def test_eq(self):
+        '''a = 1'''
+        assert self.vm.frame.hash['a'] == 1
+
+    def test_eq_expr(self):
+        '''a = 1 + 1'''
+        assert self.vm.frame.hash['a'] == 2
+
+    def test_add_eq(self):
+        '''
+        a = 2
+        a += 1
+        '''
+        assert self.vm.frame.hash['a'] == 3
+
+    def test_sub_eq(self):
+        '''
+        a = 2
+        a -= 1
+        '''
+        assert self.vm.frame.hash['a'] == 1
+
+    def test_mul_eq(self):
+        '''
+        a = 2
+        a *= 2
+        '''
+        assert self.vm.frame.hash['a'] == 4
+
+    def test_div_eq(self):
+        '''
+        a = 2
+        a /= 2
+        '''
+        assert self.vm.frame.hash['a'] == 1
