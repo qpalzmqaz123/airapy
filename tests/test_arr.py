97b6ef4f3cd8e96117001c56f5710e02f8b63ee8
wangqj <qpalzmqaz123@gmail.com>
2018-08-03 14:36:25 +0800

    add test_arr

---
+++ tests/test_arr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from .base import BaseTest
+
+class TestArr(BaseTest):
+
+    def test_arr_0(self):
+        '''
+        a = [1, 2, 3]
+        b = a[0]
+        c = a[1]
+        d = a[2]
+        '''
+        assert self.vm.frame.hash['b'] == 1
+        assert self.vm.frame.hash['c'] == 2
+        assert self.vm.frame.hash['d'] == 3
+
+    def test_arr_1(self):
+        '''
+        a = [1, 2, 3]
+        a[1] = 10
+        b = a[0]
+        c = a[1]
+        d = a[2]
+        '''
+        assert self.vm.frame.hash['b'] == 1
+        assert self.vm.frame.hash['c'] == 10
+        assert self.vm.frame.hash['d'] == 3
