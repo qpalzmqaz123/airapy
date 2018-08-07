e24ff908520c72709e84c909c79b24b19f50deef
wangqj <qpalzmqaz123@gmail.com>
2018-08-07 15:59:04 +0800

    add set_lineno for each bnf statement

--- pbl/parser.py
+++ pbl/parser.py
@@ -66,118 +66,147 @@ def p_stmt_exprs(p):
 def p_expr_stmt_identifer(p):
     '''expr_stmt : IDENTIFER'''
     p[0] = ast.Variable(p[1], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_number(p):
     '''expr_stmt : NUMBER'''
     p[0] = ast.Int(int(p[1]), line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_float(p):
     '''expr_stmt : FLOAT'''
     p[0] = ast.Float(float(p[1]), line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_string(p):
     '''expr_stmt : STRING'''
     p[0] = ast.String(p[1], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_nil(p):
     '''expr_stmt : NIL'''
     p[0] = ast.Nil(line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_true(p):
     '''expr_stmt : TRUE'''
     p[0] = ast.Boolean(True, line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_false(p):
     '''expr_stmt : FALSE'''
     p[0] = ast.Boolean(False, line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_stmt_expr_assign(p):
     '''assign_stmt : target_stmt EQUALS expr_stmt'''
     p[0] = ast.Assign(p[1], p[3])
+    p.set_lineno(0, p.lineno(1))
 
 def p_stmt_expr_add_assign(p):
     '''assign_stmt : target_stmt ADDEQ expr_stmt'''
     p[0] = ast.Assign(p[1], ast.BinOp(p[1], p[3], ast.BinOp.ADD, line=p.lineno(2), text=p[2]))
+    p.set_lineno(0, p.lineno(1))
 
 def p_stmt_expr_sub_assign(p):
     '''assign_stmt : target_stmt SUBEQ expr_stmt'''
     p[0] = ast.Assign(p[1], ast.BinOp(p[1], p[3], ast.BinOp.SUB, line=p.lineno(2), text=p[2]))
+    p.set_lineno(0, p.lineno(1))
 
 def p_stmt_expr_mul_assign(p):
     '''assign_stmt : target_stmt MULEQ expr_stmt'''
     p[0] = ast.Assign(p[1], ast.BinOp(p[1], p[3], ast.BinOp.MUL, line=p.lineno(2), text=p[2]))
+    p.set_lineno(0, p.lineno(1))
 
 def p_stmt_expr_div_assign(p):
     '''assign_stmt : target_stmt DIVEQ expr_stmt'''
     p[0] = ast.Assign(p[1], ast.BinOp(p[1], p[3], ast.BinOp.DIV, line=p.lineno(2), text=p[2]))
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_gt(p):
     '''test_stmt : expr_stmt GT expr_stmt'''
     p[0] = ast.Compare(p[1], p[3], ast.Compare.GT, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_ge(p):
     '''test_stmt : expr_stmt GE expr_stmt'''
     p[0] = ast.Compare(p[1], p[3], ast.Compare.GE, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_lt(p):
     '''test_stmt : expr_stmt LT expr_stmt'''
     p[0] = ast.Compare(p[1], p[3], ast.Compare.LT, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_le(p):
     '''test_stmt : expr_stmt LE expr_stmt'''
     p[0] = ast.Compare(p[1], p[3], ast.Compare.LE, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_eq(p):
     '''test_stmt : expr_stmt EQ expr_stmt'''
     p[0] = ast.Compare(p[1], p[3], ast.Compare.EQ, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_ne(p):
     '''test_stmt : expr_stmt NE expr_stmt'''
     p[0] = ast.Compare(p[1], p[3], ast.Compare.NE, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_and(p):
     '''test_stmt : expr_stmt AND expr_stmt'''
     p[0] = ast.Test(p[1], p[3], ast.Test.AND, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_or(p):
     '''test_stmt : expr_stmt OR expr_stmt'''
     p[0] = ast.Test(p[1], p[3], ast.Test.OR, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_not(p):
     '''test_stmt : NOT expr_stmt'''
     p[0] = ast.Test(p[2], ast.Block(), ast.Test.NOT, line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_add(p):
     '''binop_stmt : expr_stmt PLUS expr_stmt'''
     p[0] = ast.BinOp(p[1], p[3], ast.BinOp.ADD, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_sub(p):
     '''binop_stmt : expr_stmt MINUS expr_stmt'''
     p[0] = ast.BinOp(p[1], p[3], ast.BinOp.SUB, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_mul(p):
     '''binop_stmt : expr_stmt TIMES expr_stmt'''
     p[0] = ast.BinOp(p[1], p[3], ast.BinOp.MUL, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_div(p):
     '''binop_stmt : expr_stmt DIVIDE expr_stmt'''
     p[0] = ast.BinOp(p[1], p[3], ast.BinOp.DIV, line=p.lineno(2), text=p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_parenthesis(p):
     '''binop_stmt : LPAREN expr_stmt RPAREN'''
     p[0] = p[2]
+    p.set_lineno(0, p.lineno(1))
 
 def p_fn_stmt_multi_args(p):
     '''fn_stmt : FN LPAREN identifer_list IDENTIFER RPAREN DO stmt_list END'''
     p[0] = ast.Function(p[3].append(p[4]), p[7], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_fn_stmt_single_args(p):
     '''fn_stmt : FN LPAREN IDENTIFER RPAREN DO stmt_list END'''
     p[0] = ast.Function(ast.Arguments().append(p[3]), p[6], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_fn_stmt_empty_args(p):
     '''fn_stmt : FN LPAREN RPAREN DO stmt_list END'''
     p[0] = ast.Function(ast.Arguments(), p[5], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_fn_stmt_identifer_list(p):
     '''identifer_list : IDENTIFER COMMA
@@ -186,46 +215,57 @@ def p_fn_stmt_identifer_list(p):
         p[0] = ast.Arguments().append(p[1])
     elif len(p) == 4:
         p[0] = p[1].append(p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_call_stmt_multi_args(p):
     '''call_stmt : callable_stmt LPAREN expr_stmt_list expr_stmt RPAREN'''
     p[0] = ast.Call(p[1], p[3].append(p[4]), line=p.lineno(1))
+    p.set_lineno(0, p.lineno(1))
 
 def p_call_stmt_single_args(p):
     '''call_stmt : callable_stmt LPAREN expr_stmt RPAREN'''
     p[0] = ast.Call(p[1], ast.Arguments().append(p[3]), line=p.lineno(1))
+    p.set_lineno(0, p.lineno(1))
 
 def p_call_stmt_empty_args(p):
     '''call_stmt : callable_stmt LPAREN RPAREN'''
     p[0] = ast.Call(p[1], ast.Arguments(), line=p.lineno(1))
+    p.set_lineno(0, p.lineno(1))
 
 def p_array_stmt_empty_item(p):
     '''array_stmt : LSQUARE RSQUARE'''
     p[0] = ast.Array(line=p.lineno(1), text=p[1] + p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_array_stmt_single_item(p):
     '''array_stmt : LSQUARE expr_stmt RSQUARE'''
     p[0] = ast.Array().append(p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_array_stmt_multi_item(p):
     '''array_stmt : LSQUARE array_expr_stmt_list expr_stmt RSQUARE'''
     p[0] = p[2].append(p[3])
+    p.set_lineno(0, p.lineno(1))
 
 def p_hash_stmt_empty_item(p):
     '''hash_stmt : LBRACE RBRACE'''
     p[0] = ast.Hash(line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_hash_stmt_single_item(p):
     '''hash_stmt : LBRACE hash_node RBRACE'''
     p[0] = ast.Hash(line=p.lineno(1), text=p[1]).set(p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_hash_stmt_milti_items(p):
     '''hash_stmt : LBRACE hash_node_list hash_node RBRACE'''
     p[0] = p[2].set(p[3])
+    p.set_lineno(0, p.lineno(1))
 
 def p_hash_node(p):
     '''hash_node : STRING COLON expr_stmt'''
     p[0] = (ast.String(p[1], line=p.lineno(1), text=p[1]), p[3])
+    p.set_lineno(0, p.lineno(1))
 
 def p_hash_node_list(p):
     '''hash_node_list : hash_node COMMA
@@ -234,6 +274,7 @@ def p_hash_node_list(p):
         p[0] = ast.Hash(line=p.lineno(1), text=p[1]).set(p[1])
     elif len(p) == 4:
         p[0] = p[1].set(p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_property_stmt(p):
     '''property_stmt : expr_stmt LSQUARE expr_stmt RSQUARE
@@ -242,10 +283,12 @@ def p_property_stmt(p):
         p[0] = ast.Property(p[1], p[3])
     else:
         p[0] = ast.Property(p[1], ast.String(p[3], line=p.lineno(1), text=p[1]))
+    p.set_lineno(0, p.lineno(1))
 
 def p_stmt_while(p):
     '''while_stmt : WHILE expr_stmt DO stmt_list END'''
     p[0] = ast.While(p[2], p[4], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_expr_stmt_list(p):
     '''expr_stmt_list : expr_stmt COMMA
@@ -254,6 +297,7 @@ def p_expr_stmt_list(p):
         p[0] = ast.Arguments().append(p[1])
     elif len(p) == 4:
         p[0] = p[1].append(p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_array_expr_stmt_list(p):
     '''array_expr_stmt_list : expr_stmt COMMA
@@ -262,14 +306,17 @@ def p_array_expr_stmt_list(p):
         p[0] = ast.Array(line=p.lineno(1), text=p[1]).append(p[1])
     elif len(p) == 4:
         p[0] = p[1].append(p[2])
+    p.set_lineno(0, p.lineno(1))
 
 def p_if_stmt(p):
     '''if_stmt : IF expr_stmt DO stmt_list END'''
     p[0] = ast.If(p[2], p[4], ast.Block(), line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_if_else_stmt(p):
     '''if_stmt : IF expr_stmt DO stmt_list ELSE stmt_list END'''
     p[0] = ast.If(p[2], p[4], p[6], line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 # TODO elseif
 
@@ -280,14 +327,17 @@ def p_return_stmt(p):
         p[0] = ast.Return(p[2], line=p.lineno(1), text=p[1])
     else:
         p[0] = ast.Return(None, line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_break_stmt(p):
     '''break_stmt : BREAK'''
     p[0] = ast.Break(line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_continue_stmt(p):
     '''continue_stmt : CONTINUE'''
     p[0] = ast.Continue(line=p.lineno(1), text=p[1])
+    p.set_lineno(0, p.lineno(1))
 
 def p_callable_stmt_identifer(p):
     '''callable_stmt : IDENTIFER'''
@@ -299,6 +349,7 @@ def p_callable_stmt_misc(p):
                      | property_stmt
                      | fn_stmt'''
     p[0] = p[1]
+    p.set_lineno(0, p.lineno(1))
 
 def p_target_stmt(p):
     '''target_stmt : IDENTIFER
@@ -309,6 +360,7 @@ def p_target_stmt(p):
         p[0] = p[1]
     else:
         raise Exception('Invalid target_stmt')
+    p.set_lineno(0, p.lineno(1))
 
 def p_error(p):
     if p:
